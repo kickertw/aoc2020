34200a7ec7152a54712c9b8fcfa133f91f196e9c
twong <twong@reliaslearning.com>
2020-12-01 14:58:17 -0500

    Day 1 solution

---
+++ Aoc2020/Day1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace Aoc2020
+{
+    public static class Day1
+    {
+        public static int Part1(List<int> inputs, int acceptedSum = 2020)
+        {
+            for(var ii = 0; ii < inputs.Count; ii++)
+            {
+                for (var jj = 0; jj < inputs.Count; jj++)
+                {
+                    if (jj != ii && inputs[jj] + inputs[ii] == acceptedSum)
+                    {
+                        return inputs[jj] * inputs[ii];
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        public static int Part2(List<int> inputs, int acceptedSum = 2020)
+        {
+            for (var ii = 0; ii < inputs.Count; ii++)
+            {
+                for (var jj = 0; jj < inputs.Count; jj++)
+                {
+                    for (var kk = 0; kk < inputs.Count; kk++)
+                    {
+                        if (jj != ii && ii != kk && jj != kk && inputs[jj] + inputs[ii] + inputs[kk] == acceptedSum)
+                        {
+                            return inputs[jj] * inputs[ii] * inputs[kk];
+                        }
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Aoc2020/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Aoc2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = File.ReadLines("inputs/day1.txt").Select(line => int.Parse(line)).ToList();
+
+            Console.WriteLine($"Part 1 answer = {Day1.Part1(inputs)}");
+            Console.WriteLine($"Part 2 answer = {Day1.Part2(inputs)}");
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+        }
+    }
+}
