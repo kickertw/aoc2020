4f20229129cefa7b9843d11eaba145a051d66e64
Tim Wong <k1ck3rtw@gmail.com>
2020-12-05 12:08:01 -0500

    day 5 solutions

---
+++ Aoc2020/Day5.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Aoc2020
+{
+    public static class Day5
+    {
+        public static int Part1(IEnumerable<string> rawInputs)
+        {
+            var seatId = 0;
+
+            foreach (var input in rawInputs)
+            {
+                var tempId = GetSeatId(input);
+                seatId = seatId < tempId ? tempId : seatId;
+            }
+
+            return seatId;
+        }
+
+        public static int Part2(IEnumerable<string> rawInputs)
+        {
+            var seatIdList = new List<int>();
+
+            foreach (var input in rawInputs)
+            {
+                seatIdList.Add(GetSeatId(input));
+            }
+
+            seatIdList.Sort();
+            for(var ii = 0; ii < seatIdList.Count; ii++)
+            {
+                if (seatIdList[ii] + 2 == seatIdList[ii + 1])
+                {
+                    return seatIdList[ii] + 1;
+                }
+            }
+
+            // We should never get here;
+            return 0;
+        }
+
+        private static int GetSeatId(string input)
+        {
+            var min = 0;
+            var max = 127;
+            var row = 0;
+            var col = 0;
+
+            foreach (var letter in input)
+            {
+                if ((letter == 'L' || letter == 'R') && min == max)
+                {
+                    row = min;
+                    min = 0;
+                    max = 7;
+                }
+
+                switch (letter)
+                {
+                    case 'F':
+                    case 'L':
+                        //Console.Write($"max = {max} =>");
+                        max -= ((max + 1 - min) / 2);
+                        //Console.Write($" {max}");
+                        break;                    
+                    case 'B':
+                    case 'R':
+                        //Console.Write($"min = {min} =>");
+                        min += ((max + 1 - min) / 2);
+                        //Console.Write($"{min}");
+                        break;
+                }
+                //Console.WriteLine($"    min={min} max={max}");
+            }
+
+            col = min;
+
+            //Console.WriteLine($"row={row} col={col}");
+            //Console.WriteLine("");
+            return row * 8 + col;
+        }
+    }
+}
--- Aoc2020/Program.cs
+++ Aoc2020/Program.cs
@@ -9,10 +9,10 @@ namespace Aoc2020
     {
         static void Main(string[] args)
         {
-            var inputs = File.ReadLines("inputs/day4.txt");
+            var inputs = File.ReadLines("inputs/day5.txt");
 
-            Console.WriteLine($"Part 1 answer = {Day4.Part1(inputs)}");
-            Console.WriteLine($"Part 2 answer = {Day4.Part2(inputs)}");
+            Console.WriteLine($"Part 1 answer = {Day5.Part1(inputs)}");
+            Console.WriteLine($"Part 2 answer = {Day5.Part2(inputs)}");
             Console.WriteLine("Press enter to continue");
             Console.ReadLine();
         }
