ba4799593431ddb17873a487d82e25766b7ebf1a
Tim Wong <k1ck3rtw@gmail.com>
2020-12-06 14:39:54 -0500

    Day 6 solution

---
+++ Aoc2020/Day6.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aoc2020
+{
+    public static class Day6
+    {
+        public static int Part1(IEnumerable<string> rawInputs, bool forPart2 = false)
+        {
+            var retVal = 0;
+            var newGroup = true;
+            var groupAnswers = new HashSet<char>();
+
+            foreach (string input in rawInputs)
+            {
+                if (input.Length == 0)
+                {
+                    //Console.WriteLine("");
+                    retVal += groupAnswers.Count;
+                    groupAnswers.Clear();
+                    newGroup = true;
+                    continue;
+                }
+
+                if (forPart2)
+                {
+                    if (newGroup)
+                    {
+                        groupAnswers.UnionWith(input.ToList());
+                        //Console.WriteLine($"New Group starting at [{string.Join(",", groupAnswers)}]");
+                        newGroup = false;
+                    }
+
+                    //Console.Write($"    Before = {string.Join(",", groupAnswers)} / ");
+                    groupAnswers.IntersectWith(input.ToList());
+                    //Console.WriteLine($"After = {string.Join(",", groupAnswers)} / ");
+                }
+                else
+                {
+                    groupAnswers.UnionWith(input.ToList());                    
+                }
+            }
+
+            return retVal + groupAnswers.Count;
+        }
+
+        public static int Part2(IEnumerable<string> rawInputs)
+        {
+            return Part1(rawInputs, true);
+        }
+    }
+}
--- Aoc2020/Program.cs
+++ Aoc2020/Program.cs
@@ -7,10 +7,10 @@ namespace Aoc2020
     {
         static void Main(string[] args)
         {
-            var inputs = File.ReadLines("inputs/day5.txt");
+            var inputs = File.ReadLines("inputs/day6.txt");
 
-            Console.WriteLine($"Part 1 answer = {Day5.Part1(inputs)}");
-            Console.WriteLine($"Part 2 answer = {Day5.Part2(inputs)}");
+            Console.WriteLine($"Part 1 answer = {Day6.Part1(inputs)}");
+            Console.WriteLine($"Part 2 answer = {Day6.Part2(inputs)}");
             Console.WriteLine("Press enter to continue");
             Console.ReadLine();
         }
