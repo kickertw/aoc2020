e909b51f188e6a359039ebe335ca1eadc0cd7ec4
twong <twong@reliaslearning.com>
2020-12-16 09:27:29 -0500

    Day 15 solutions

---
+++ Aoc2020/Day15.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aoc2020
+{
+    //--- Day 14: Docking Data ---
+    public static class Day15
+    {
+        public static int Part1(IEnumerable<string> rawInputs, int turnToGet = 2020)
+        {
+            var idx = 1;
+            var inputs = rawInputs.First().Split(',');
+            var spokenNumberCounter = inputs.ToDictionary(input => int.Parse(input), input => new SpokenNumberTracker(idx++));
+
+            var lastSpokenNumber = int.Parse(inputs.Last());
+            for (var ii = spokenNumberCounter.Count() + 1; ii <= turnToGet; ii++)
+            {
+                if (spokenNumberCounter.ContainsKey(lastSpokenNumber))
+                {
+                    if (spokenNumberCounter[lastSpokenNumber].LastTurnSpoken == spokenNumberCounter[lastSpokenNumber].LastLastTurnSpoken)
+                    {
+                        lastSpokenNumber = 0;
+                    }
+                    else
+                    {
+                        lastSpokenNumber = spokenNumberCounter[lastSpokenNumber].LastTurnSpoken -
+                                           spokenNumberCounter[lastSpokenNumber].LastLastTurnSpoken;
+                    }
+                }
+
+                if (spokenNumberCounter.ContainsKey(lastSpokenNumber))
+                {
+                    spokenNumberCounter[lastSpokenNumber].LastLastTurnSpoken = spokenNumberCounter[lastSpokenNumber].LastTurnSpoken;
+                    spokenNumberCounter[lastSpokenNumber].LastTurnSpoken = ii;
+                }
+                else
+                {
+                    spokenNumberCounter.Add(lastSpokenNumber, new SpokenNumberTracker(ii));
+                }
+            }
+
+            return lastSpokenNumber;
+        }
+
+        // There is a more efficient way, this is just brute forcing :(
+        public static long Part2(IEnumerable<string> rawInputs)
+        {
+            return Part1(rawInputs, 30000000);
+        }
+    }
+
+    public class SpokenNumberTracker
+    {
+        public int LastTurnSpoken { get; set; }
+        public int LastLastTurnSpoken { get; set; }
+
+        public SpokenNumberTracker(int lastTurnSpoken)
+        {
+            LastTurnSpoken = lastTurnSpoken;
+            LastLastTurnSpoken = lastTurnSpoken;
+        }
+
+        public void UpdateLastTurn(int currentTurn)
+        {
+            LastLastTurnSpoken = LastTurnSpoken;
+            LastTurnSpoken = currentTurn;
+        }
+    }
+}
